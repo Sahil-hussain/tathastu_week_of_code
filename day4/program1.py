7cdb41aad34c037f328c9cc42ba41abd22ec0fc9
sachin628 <60085530+sachin628@users.noreply.github.com>
2020-06-06 17:49:08 +0530

    Create program1.py

---
+++ day4/program1.py
@@ -0,0 +1,9 @@
+# count occurences of an elements in a Tuple
+
+print(" enter numbers separated by comma :")
+t1=tuple([int(e) for e in input().split(',')])
+i=0
+for e in t1:
+    if i==t1.index(e):
+        print(e,' , ',t1.count(e))
+    i+=1   
