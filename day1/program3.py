488abd427ccdde4b813d80aba39b50de62449623
sachin628 <60085530+sachin628@users.noreply.github.com>
2020-06-06 17:12:10 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+x=x+y
+y=x-y
+x=x-y
+print("After the swapping: ")
+print("value of x=", x)
+print("value of y=",y)
